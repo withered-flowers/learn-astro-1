6b287ad07fd693a758f320bcce11d3ceb5a8dbda
Withered Flowers <withered-flowers@users.noreply.github.com>
2022-04-06 06:48:02 +0700

    feat: add svelte and create PostTable svelte component

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 import { defineConfig } from "astro/config";
-
 import tailwind from "@astrojs/tailwind";
 import netlify from "@astrojs/netlify/functions";
 
+import svelte from "@astrojs/svelte";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind(), svelte()],
   adapter: netlify(),
   // When using adapter netlify, NEED to specify site or will get error "INVALID URL"
-  site: "https://learn-astro-ssr.netlify.app",
+  site: "https://learn-astro-ssr.netlify.app"
 });
---
+++ src/components/PostTable.svelte
@@ -0,0 +1,24 @@
+<script>
+  export let posts;
+</script>
+
+<table>
+  <thead>
+    <tr>
+      <th class="border border-slate-600">Id</th>
+      <th class="border border-slate-600">UserId</th>
+      <th class="border border-slate-600">Title</th>
+      <th class="border border-slate-600">Body</th>
+    </tr>
+  </thead>
+  <tbody>
+    {#each posts as post}
+      <tr>
+        <td class="border border-slate-600">{post.id}</td>
+        <td class="border border-slate-600">{post.userId}</td>
+        <td class="border border-slate-600">{post.title}</td>
+        <td class="border border-slate-600">{post.body}</td>
+      </tr>
+    {/each}
+  </tbody>
+</table>
